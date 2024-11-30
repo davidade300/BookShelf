dc3cef3cb0eda17363f5f7ce1fa915132748c962
davidade300 <iaderinho@icloud.com>
2024-11-30 01:03:32 -0300

    changed test values for the sake of reality.

--- bookshelf/model.py
+++ bookshelf/model.py
@@ -1,3 +1,4 @@
+from dataclasses import dataclass
 from enum import Enum
 
 
@@ -32,15 +33,20 @@ class Book:
         return hash(self.isbn)
 
 
+@dataclass(unsafe_hash=True)
 class Category:
-    def __init__(self, name: str, book_isbn: int) -> None:
-        self.name = name
-        self.book_isbn = book_isbn
+    name: str
+    book_isbn: int
 
-    def __eq__(self, other) -> bool:
-        if not isinstance(other, Category):
-            return False
-        return other.book_isbn == self.book_isbn and other.name == self.name
 
-    def __hash__(self) -> int:
-        return hash(self.name.casefold())
+# def __init__(self, name: str, book_isbn: int) -> None:
+# self.name = name
+# self.book_isbn = book_isbn
+#
+# def __eq__(self, other) -> bool:
+# if not isinstance(other, Category):
+# return False
+# return other.book_isbn == self.book_isbn and other.name == self.name
+#
+# def __hash__(self) -> int:
+# return hash(self.name.casefold())
--- tests/test_instantiate.py
+++ tests/test_instantiate.py
@@ -1,12 +1,7 @@
-import pytest  # noqa: F401
 from bookshelf.model import Book, Status
 
 
 def test_creating_book_with_empty_status():
-    empty_status_book = Book(
-        "title",
-        "portugues",
-        "213456987",
-    )
+    empty_status_book = Book("DDD", "Portuguese", 9788550800653)
 
     assert empty_status_book.status is Status.NOT_STARTED
